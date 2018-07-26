0ff875a81a51e54cda58d6d5968cfceab2dd9efb
Amy Kwon <19kwony@mbusdapps.org>
2018-07-26 13:55:03 -0700

    hjhj

--- menu.js
+++ menu.js
@@ -1,22 +1,45 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+var nav = document.getElementById("topNav");
+var main = document.getElementById("main");
+var menu = document.getElementsByClassName("menuitems");
+var close = document.getElementById("closebtn");
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+//default to measure if/else from
+nav.style.height = "50px";
+main.style.marginTop = "50px";
+for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+close.addEventListener("click", function(){
+  var menuIcon = close.children;
+  for (i = 0; i < menuIcon.length; i++){
+  menuIcon[i].classList.toggle("active");
   }
-}
+});
+
+function navToggle() {
+	//to close
+	if (nav.style.height <= "275px") {
+	nav.style.height = "50px";
+	main.style.marginTop = "50px";
+
+    	var i = 0;
+    	for (i = 0; i < menu.length; i++){
+	menu[i].style.opacity="0.0";
+	menu[i].style.marginTop="100px";
+	};
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+
+	}
+	//to open
+	else if (nav.style.height <= "50px") {
+	nav.style.height = "275px";
+	main.style.marginTop = "275px";
+    	var i = 0;
+    	for (i = 0; i < menu.length; i++){
+	menu[i].style.opacity="1.0";
+	menu[i].style.marginTop="0px";
+	};
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+	}
+
+};
